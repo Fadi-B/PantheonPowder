613946003a47f4ed9c21ce3f794dba1f7e040fd4
Fadi-B <64134035+Fadi-B@users.noreply.github.com>
2023-01-28 15:27:03 +0100

    Adding permissions for Sprout-Fadi

--- scripts/setup_framework.sh
+++ scripts/setup_framework.sh
@@ -10,6 +10,7 @@ chmod 755 sprout-ewma.py
 chmod 755 sprout-ma.py
 chmod 755 sprout-sewma.py
 chmod 755 sprout-oracle.py
+chmod 755 sprout-fadi.py
 
 cd ..
 cd ..
@@ -18,5 +19,6 @@ chmod -R 755 third_party/sprout-ewma/
 chmod -R 755 third_party/sprout-ma/
 chmod -R 755 third_party/sprout-sewma/
 chmod -R 755 third_party/sprout-oracle/
+chmod -R 755 third_party/sprout-fadi/
 
 src/experiments/setup.py --schemes "cubic sprout vivace verus copa"
