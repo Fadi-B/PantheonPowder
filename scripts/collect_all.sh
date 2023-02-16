1c26bb5b4ce49b5b61f83e03be83f140c733ad86
Fadi-B <64134035+Fadi-B@users.noreply.github.com>
2023-02-16 19:50:16 +0100

    Create collect_all.sh

---
+++ scripts/collect_all.sh
@@ -0,0 +1,18 @@
+cd pantheon/
+
+var=$(cat traces.txt)
+
+CC_SCHEMES="bbr copa cubic fillp fillp_sheep indigo ledbat pcc pcc_experimental sprout taova vegas verus vivace"
+
+TIME=30
+RUNS=1
+
+sudo src/experiments/setup.py --setup --schemes "$CC_SCHEMES"
+
+for line in $var
+do
+   mkdir "${line}"
+   src/experiments/test.py local --schemes "$CC_SCHEMES" --uplink-trace /usr/share/mahimahi/traces/${line}-driving.up --downlink-trace /usr/share/mahimahi/traces/${line}-driving.down --data-dir ${line} -t ${TIME} --run-times ${RUNS} --pkill-cleanup
+   src/analysis/analyze.py --data-dir ${line}
+done
+
